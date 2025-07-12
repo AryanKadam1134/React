9bdaaf8313a9902420493c6d8f52fd408ce21814
AryanKadam1134 <aryankadam3114@gmail.com>
2025-07-12 23:54:23 +0530

    todolist

--- 10_todocontextLocal/src/App.jsx
+++ 10_todocontextLocal/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useEffect } from 'react'
+import { TodoContext, TodoProvider } from './context'
+import TodoForm from './components/TodoFrom';
+import TodoItem from './components/TodoItem';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = React.useState([]);
+
+  const addTodo = (task) => {
+    setTodos((prev) => [...prev, {...task}])
+  }
+
+  const updateTodo = (id, task) => {
+    setTodos( (prev) => prev.map( (prevTodo) => (prevTodo.id == id ? task : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos( (prev) => prev.filter( (prevTodo) => (prevTodo.id !== id)))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos( (prev) => prev.map( (prevTodo) => (prevTodo.id == id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo)))
+  }
+
+  useEffect( () => {
+    const localTodos = JSON.parse(localStorage.getItem("localTodos"))
+
+    if (localTodos && localTodos.length > 0) {
+      setTodos(localTodos)
+    }
+  }, [])
+
+  useEffect( () => {
+    localStorage.setItem("localTodos", JSON.stringify(todos))
+  }, [todos])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+            <div className="mb-4">
+                {/* Todo form goes here */}
+                <TodoForm />
+            </div>
+            <div className="flex flex-wrap gap-y-3">
+                {/*Loop and Add TodoItem here */}
+                {todos.map((todo) => (
+                    <TodoItem key={todo.id} todo={todo} />
+                ))}
+
+            </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </TodoProvider>
   )
 }
 
